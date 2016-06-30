04b66d1adf562ffd0ac0ca3a9a018f53963276fa
CJBarry <cjb309@bham.ac.uk>
2016-06-30 16:29:17 +0100

    removed unnecessary test

--- mod_tests.py
+++ mod_tests.py
@@ -1,9 +1,1 @@
 # This is for you to fill in!
-
-from mean import *
-
-def test_mean_1234():
-	num_list = [1, 2, 3, 4]
-	calc_mean = mean(num_list)
-	expected_mean = 2.5
-	assert expected_mean == calc_mean
