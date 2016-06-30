7ed27b473fe7417d6dca22065c817fb2bbbbe0f4
CJBarry <cjb309@bham.ac.uk>
2016-06-30 16:24:42 +0100

    corrected integers to floats for Python 2.7 compatibility

--- mean_tests.py
+++ mean_tests.py
@@ -7,14 +7,14 @@ def test_ints():
     assert obs == exp
 
 def test_zero():
-    num_list=[0,2,4,6]
+    num_list=[0.,2.,4.,6.]
     obs = mean(num_list)
     exp = 3
     assert obs == exp
 
 def test_double():
     # This one will fail in Python 2
-    num_list=[1,2,3,4]
+    num_list=[1.,2.,3.,4.]
     obs = mean(num_list)
     exp = 2.5
     assert obs == exp
